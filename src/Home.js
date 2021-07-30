0db511495f2e8629dc59a576faad276eef0a5173
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-07-30 18:08:26 +0545

    refactor code to use data from json server instead of hardcoded values

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null)
 
   const deleteBlog = (id) => {
       const newData = blogs.filter((blog) => blog.id !== id)
       setBlogs(newData)
   }
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+    .then( res => {
+        return res.json()
+    })
+    .then( resarray => setBlogs(resarray))
+  }, [])
   return (
     <div className="home">
-        <BlogList blogs={blogs} title="All Blogs" deleteBlog={deleteBlog} />
-        <BlogList blogs={blogs.filter( (blog) => blog.author ===  'mario')} title="Mario's Blogs" deleteBlog={deleteBlog} />
+        { blogs && <BlogList blogs={blogs} title="All Blogs" deleteBlog={deleteBlog} />}
+        {/* <BlogList blogs={blogs.filter( (blog) => blog.author ===  'mario')} title="Mario's Blogs" deleteBlog={deleteBlog} /> */}
         
     </div>
   );
