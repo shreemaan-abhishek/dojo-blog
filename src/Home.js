84e79cee53f5cc21e57d9f798393ebee63d1b6c4
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-07-30 20:33:58 +0545

    implement better error handling code

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,35 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
+    const [blogs, setBlogs] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-    .then( res => {
-        return res.json()
-    })
-    .then( resarray => {
-        setBlogs(resarray)
-        setIsLoading(false)
-    })
-  }, [])
-  return (
-    <div className="home">
-        { isLoading && <div>Loading...</div> }
-        { blogs && <BlogList blogs={blogs} title="All Blogs" />}
-        
-    </div>
-  );
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setIsLoading(false);
+                setBlogs(data);
+                setError(null);
+            })
+            .catch(err => {
+                setIsLoading(false);
+                setError(err.message);
+            })
+    }, [])
+    return (
+        <div className="home">
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+
+        </div>
+    );
 }
- 
+
 export default Home;
