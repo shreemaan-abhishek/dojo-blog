5c69fd273838c6e6ffea4b47a58ffd97dde71cd8
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-08-01 20:25:27 +0545

    refactor code to use React links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom'
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New</Link>
             </div>
         </nav>
      );
