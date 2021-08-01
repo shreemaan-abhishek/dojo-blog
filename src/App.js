1d6fb4c1d1d9eeaf77b196d8d9e13d54a4f1d7a6
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-08-01 17:01:30 +0545

    use react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from "./Navbar"
 import Home from "./Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-        <Navbar/>
-      <div className="content">
-          <Home/>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    <Switch>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
