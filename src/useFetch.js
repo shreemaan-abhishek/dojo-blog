3f18802fe25c289e8a247acf91e2b50d5174974f
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-08-01 20:26:09 +0545

    refactore code to use abort controller

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,28 +1,38 @@
 import { useEffect, useState } from "react";
+const abortController = new AbortController()
 const useFetch = (url) => {
     const [data, setData] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null);
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('could not fetch the data for that resource');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setIsLoading(false);
-                setData(data);
-                setError(null);
-            })
-            .catch(err => {
-                setIsLoading(false);
-                setError(err.message);
-            })
+        setTimeout(() => {
+            fetch(url, { signal: abortController.signal })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setIsLoading(false);
+                    setData(data);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted')
+                    } else {
+                        setIsLoading(false);
+                        setError(err.message);
+                    }
+                })
+
+            return () => abortController.abort
+        }, 1000)
+
     }, [url])
 
-    return {data, isLoading, error}
+    return { data, isLoading, error }
 }
 
 export default useFetch
